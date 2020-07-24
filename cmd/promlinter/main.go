200b7955aa10ad7625027bf5670ddd86da3fc63b
Ben Ye <yb532204897@gmail.com>
2020-07-23 22:25:37 -0400

    add --disable flag to disable lint functions
    Signed-off-by: Ben Ye <yb532204897@gmail.com>

--- cmd/promlinter/main.go
+++ cmd/promlinter/main.go
@@ -14,14 +14,42 @@ import (
 	"github.com/yeya24/promlinter"
 )
 
+const help = `Prometheus metrics linter for Go code.
+
+This tool can cover most of the patterns of metrics naming issues, but it cannot detect metric values that can only be determined in the runtime.
+
+By default it doesn' output parsing failures, if you want to see them, you can add --strict flag to enable it.
+
+It is also supported to disable the lint functions using repeated flag --disable. Current supported functions are:
+
+	[Help]: Help detects issues related to the help text for a metric.
+
+	[MetricUnits]: MetricUnits detects issues with metric unit names.
+
+	[Counter]: Counter detects issues specific to counters, as well as patterns that should only be used with counters.
+
+	[HistogramSummaryReserved]: HistogramSummaryReserved detects when other types of metrics use names or labels reserved for use by histograms and/or summaries.
+
+	[MetricTypeInName]: MetricTypeInName detects when metric types are included in the metric name.
+
+	[ReservedChars]: ReservedChars detects colons in metric names.
+
+	[CamelCase]: CamelCase detects metric names and label names written in camelCase.
+
+	[UnitAbbreviations]: UnitAbbreviations detects abbreviated units in the metric name.
+`
+
 func main() {
-	app := kingpin.New(filepath.Base(os.Args[0]), "Prometheus metrics linter tool for golang.")
+	app := kingpin.New(filepath.Base(os.Args[0]), help)
 	app.Version("v0.0.1")
 	app.HelpFlag.Short('h')
 
 	paths := app.Arg("files", "Files to lint.").Strings()
 	strict := app.Flag("strict", "Strict mode. If true, linter will output more issues including parsing failures.").
 		Default("false").Short('s').Bool()
+	disableLintFuncs := app.Flag("disable", "Disable lint functions (repeated)."+
+		"Supported options: Help, Counter, MetricUnits, HistogramSummaryReserved, MetricTypeInName, "+
+		"ReservedChars, CamelCase, UnitAbbreviations").Short('d').Enums(promlinter.LintFuncNames...)
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
@@ -41,7 +69,8 @@ func main() {
 		}
 	}
 
-	for _, iss := range promlinter.Run(fileSet, files, *strict) {
+	setting := promlinter.Setting{Strict: *strict, DisabledLintFuncs: *disableLintFuncs}
+	for _, iss := range promlinter.Run(fileSet, files, setting) {
 		fmt.Printf("%s %s %s\n", iss.Pos, iss.Metric, iss.Text)
 	}
 }
