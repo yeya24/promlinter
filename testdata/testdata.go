c22e49542017cf4e8dab5771d83c82c9cf612096
Ben Ye <yb532204897@gmail.com>
2020-12-03 13:48:42 -0500

    update testdata
    Signed-off-by: Ben Ye <yb532204897@gmail.com>

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -4,6 +4,7 @@ package testdata
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"k8s.io/kube-state-metrics/v2/pkg/metric"
 	generator "k8s.io/kube-state-metrics/v2/pkg/metric_generator"
 )
@@ -95,94 +96,94 @@ var (
 
 
 func main() {
-	//ch := make(chan<- prometheus.Metric)
-	//
-	//// counter metric should have _total suffix
-	//_ = promauto.NewCounterVec(
-	//	prometheus.CounterOpts{
-	//		Name: "test_metric_name",
-	//		Help: "test help text",
-	//	},
-	//	[]string{},
-	//)
-	//
-	//// no help text
-	//_ = promauto.NewCounterVec(
-	//	prometheus.CounterOpts{
-	//		Name: "test_metric_total",
-	//	},
-	//	[]string{},
-	//)
-	//
-	//// NewCounterFunc, should have _total suffix
-	//_ = promauto.NewCounterFunc(prometheus.CounterOpts{
-	//	Name: "foo",
-	//	Help: "bar",
-	//}, func() float64 {
-	//	return 1
-	//})
-	//
-	//// good
-	//f := promauto.With(prometheus.NewRegistry())
-	//_ = f.NewCounterVec(
-	//	prometheus.CounterOpts{
-	//		Name: "test_metric_total",
-	//		Help: "",
-	//	},
-	//	[]string{},
-	//)
-	//
-	//// good
-	//_ = prometheus.NewCounterVec(
-	//	prometheus.CounterOpts{
-	//		Name: "test_metric_total",
-	//		Help: "",
-	//	},
-	//	[]string{},
-	//)
-	//
-	//// good
-	//desc := prometheus.NewDesc(
-	//	"prometheus_operator_spec_replicas",
-	//	"Number of expected replicas for the object.",
-	//	[]string{
-	//		"namespace",
-	//		"name",
-	//	}, nil,
-	//)
-	//ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, 1)
-	//
-	//// support using BuildFQName to generate fqName here.
-	//// bad metric, gauge shouldn't have _total
-	//ch <- prometheus.MustNewConstMetric(prometheus.NewDesc(
-	//	prometheus.BuildFQName("foo", "bar", "total"),
-	//	"Number of expected replicas for the object.",
-	//	[]string{
-	//		"namespace",
-	//		"name",
-	//	}, nil), prometheus.GaugeValue, 1)
-	//
-	//// support detecting kubernetes metrics
-	//kubeMetricDesc := metrics.NewDesc(
-	//	"kube_test_metric_count",
-	//	"Gauge Help",
-	//	[]string{}, nil, metrics.STABLE, "",
-	//)
-	//ch <- metrics.NewLazyConstMetric(kubeMetricDesc, metrics.GaugeValue, 1)
-	//
-	//// bad
-	//_ = metrics.NewHistogram(&metrics.HistogramOpts{
-	//	Name: "test_histogram_duration_seconds",
-	//	Help: "",
-	//})
-	//
-	//// https://github.com/prometheus/mysqld_exporter/blob/master/collector/engine_innodb.go#L78-L82
-	//// This is not supported because we cannot infer what newDesc is doing before runtime.
-	//ch <- prometheus.MustNewConstMetric(
-	//	newDesc("innodb", "queries_inside_innodb", "Queries inside InnoDB."),
-	//	prometheus.GaugeValue,
-	//	1,
-	//)
+	ch := make(chan<- prometheus.Metric)
+
+	// counter metric should have _total suffix
+	_ = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_metric_name",
+			Help: "test help text",
+		},
+		[]string{},
+	)
+
+	// no help text
+	_ = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_metric_total",
+		},
+		[]string{},
+	)
+
+	// NewCounterFunc, should have _total suffix
+	_ = promauto.NewCounterFunc(prometheus.CounterOpts{
+		Name: "foo",
+		Help: "bar",
+	}, func() float64 {
+		return 1
+	})
+
+	// good
+	f := promauto.With(prometheus.NewRegistry())
+	_ = f.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_metric_total",
+			Help: "",
+		},
+		[]string{},
+	)
+
+	// good
+	_ = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_metric_total",
+			Help: "",
+		},
+		[]string{},
+	)
+
+	// good
+	desc := prometheus.NewDesc(
+		"prometheus_operator_spec_replicas",
+		"Number of expected replicas for the object.",
+		[]string{
+			"namespace",
+			"name",
+		}, nil,
+	)
+	ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, 1)
+
+	// support using BuildFQName to generate fqName here.
+	// bad metric, gauge shouldn't have _total
+	ch <- prometheus.MustNewConstMetric(prometheus.NewDesc(
+		prometheus.BuildFQName("foo", "bar", "total"),
+		"Number of expected replicas for the object.",
+		[]string{
+			"namespace",
+			"name",
+		}, nil), prometheus.GaugeValue, 1)
+
+	// support detecting kubernetes metrics
+	kubeMetricDesc := metrics.NewDesc(
+		"kube_test_metric_count",
+		"Gauge Help",
+		[]string{}, nil, metrics.STABLE, "",
+	)
+	ch <- metrics.NewLazyConstMetric(kubeMetricDesc, metrics.GaugeValue, 1)
+
+	// bad
+	_ = metrics.NewHistogram(&metrics.HistogramOpts{
+		Name: "test_histogram_duration_seconds",
+		Help: "",
+	})
+
+	// https://github.com/prometheus/mysqld_exporter/blob/master/collector/engine_innodb.go#L78-L82
+	// This is not supported because we cannot infer what newDesc is doing before runtime.
+	ch <- prometheus.MustNewConstMetric(
+		newDesc("innodb", "queries_inside_innodb", "Queries inside InnoDB."),
+		prometheus.GaugeValue,
+		1,
+	)
 }
 
 func newDesc(subsystem, name, help string) *prometheus.Desc {
